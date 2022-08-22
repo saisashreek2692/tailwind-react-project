601ef2c9490a35bb2ec9ef3250a2871b26192572
D Sai Sashreek <saisashreekd@gmail.com>
2022-08-22 15:59:31 +0530

    New Components created under Components folder

--- tailwindreact/src/App.js
+++ tailwindreact/src/App.js
@@ -1,24 +1,16 @@
 import React from "react";
-import "./App.css";
+import About from "./Components/About";
+import Hero from "./Components/Hero";
+import Navbar from "./Components/Navbar";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+    </>
   );
-}
+};
 
 export default App;
